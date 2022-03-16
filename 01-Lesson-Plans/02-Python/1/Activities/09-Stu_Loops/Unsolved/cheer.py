fc0aecc6b4f35434512a01d056fa2233b79880d3
Michael Foley <96324206+themichaelfoley@users.noreply.github.com>
2022-03-16 17:09:56 -0400

    Python class activites 07 and 09

--- 01-Lesson-Plans/02-Python/1/Activities/09-Stu_Loops/Unsolved/cheer.py
+++ 01-Lesson-Plans/02-Python/1/Activities/09-Stu_Loops/Unsolved/cheer.py
@@ -1,12 +1,15 @@
 # Create a variable named cheer and give it a word to cheer (i.e. Python or FinTech)
-
+cheer= "Python"
 
 # Below strings can be used to add fun
 cheer_symbol = "*\O/*"
 cheer_symbol_2 = "ヘ( ^o^)ノ＼(^_^ )"
 
 # Loop through string and print each letter with a cheer
+chant_loop = "Python"
 
+for i in range(len(chant_loop)):
+    print (chant_loop[i])
 
 # Print excitement to screen
 print("\nWhat does that spell?!")
