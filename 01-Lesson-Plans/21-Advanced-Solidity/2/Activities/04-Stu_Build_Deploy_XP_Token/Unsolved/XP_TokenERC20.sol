c8f2a2b8f11ff1600f4d9cc503171dddc04c23c8
Michael Foley <96324206+themichaelfoley@users.noreply.github.com>
2022-08-10 17:00:19 -0400

    Added 21 HW and project 3

--- 01-Lesson-Plans/21-Advanced-Solidity/2/Activities/04-Stu_Build_Deploy_XP_Token/Unsolved/XP_TokenERC20.sol
+++ 01-Lesson-Plans/21-Advanced-Solidity/2/Activities/04-Stu_Build_Deploy_XP_Token/Unsolved/XP_TokenERC20.sol
@@ -8,18 +8,22 @@ contract XP_Token is ERC20, ERC20Detailed {
 
     modifier onlyOwner {
         // @TODO: add a `require` to check if `owner` is the `msg.sender`
+        require(msg.sender == owner, "You do not have permission to mint these tokens!");
         _; // this underscore sends us back to the function that called this modifier
     }
 
     // @TODO: Pass the required parameters to `ERC20Detailed`
-    constructor(uint initial_supply) ERC20Detailed() public {
+    constructor(uint initial_supply) ERC20Detailed("XP_token, "XPT", 18) public {
         // @TODO: Set the owner to be `msg.sender`
+        owner = msg.sender;
 
         // @TODO: Call the internal `_mint` function to give `initial_supply` to the `owner`
+        _mint(owner, initial_supply);
     }
 
     // @TODO: Add the `onlyOwner` modifier to this function after `public`
-    function mint(address recipient, uint amount) public {
+    function mint(address recipient, uint amount) public {onlyOwner {
+        _mint(recipient, amount);
         // @TODO: Call the internal `_mint` function and pass the `recipient` and `amount` variables
     }
 }
